80d1836f11fbffbd6c501f5f20bc16feba906ad1
durbcodes <nick.durbin@gmail.com>
2020-06-27 12:52:28 -0400

    Brought in a doubly_linked_list for the ring_buffer.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,12 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+	def __init__(self, capacity):
+		self.capacity = capacity
+		self.current = None
+		self.storage = []
 
-    def append(self, item):
-        pass
+	def append(self, item):
+		if len(self.storage) < self.capacity:
+					
 
-    def get(self):
-        pass
+	def get(self):
+		pass
